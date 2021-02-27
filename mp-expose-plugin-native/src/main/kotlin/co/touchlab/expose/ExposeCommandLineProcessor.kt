b081995c813feb0332057346597c8e9da0adf2ee
Brady Aiello <brady@touchlab.co>
2021-02-26 16:42:19 -0800

    Add back mp-expose-plugin-native src/ which were part of the gitignore.

---
+++ mp-expose-plugin-native/src/main/kotlin/co/touchlab/expose/ExposeCommandLineProcessor.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2020 Brian Norman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.touchlab.expose
+
+import com.google.auto.service.AutoService
+import org.jetbrains.kotlin.compiler.plugin.AbstractCliOption
+import org.jetbrains.kotlin.compiler.plugin.CliOption
+import org.jetbrains.kotlin.compiler.plugin.CommandLineProcessor
+import org.jetbrains.kotlin.config.CompilerConfiguration
+import org.jetbrains.kotlin.config.CompilerConfigurationKey
+
+@AutoService(CommandLineProcessor::class)
+class ExposeCommandLineProcessor : CommandLineProcessor {
+  companion object {
+    private const val OPTION_ENABLED = "enabled"
+
+    val ARG_ENABLED = CompilerConfigurationKey<Boolean>(OPTION_ENABLED)
+  }
+
+  override val pluginId: String = BuildConfig.KOTLIN_PLUGIN_ID
+
+  override val pluginOptions: Collection<CliOption> = listOf(
+    CliOption(
+      optionName = OPTION_ENABLED,
+      valueDescription = "bool <true | false>",
+      description = "If the DebugLog annotation should be applied",
+      required = false,
+    ),
+  )
+
+  override fun processOption(
+    option: AbstractCliOption,
+    value: String,
+    configuration: CompilerConfiguration
+  ) {
+    return when (option.optionName) {
+      OPTION_ENABLED -> configuration.put(ARG_ENABLED, value.toBoolean())
+      else -> throw IllegalArgumentException("Unexpected config option ${option.optionName}")
+    }
+  }
+}
---
+++ mp-expose-plugin-native/src/main/kotlin/co/touchlab/expose/ExposeComponentRegistrar.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2020 Brian Norman
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package co.touchlab.expose
+
+import com.google.auto.service.AutoService
+import com.intellij.mock.MockProject
+import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
+import org.jetbrains.kotlin.cli.common.CLIConfigurationKeys
+import org.jetbrains.kotlin.cli.common.messages.MessageCollector
+import org.jetbrains.kotlin.compiler.plugin.ComponentRegistrar
+import org.jetbrains.kotlin.config.CompilerConfiguration
+
+@AutoService(ComponentRegistrar::class)
+class ExposeComponentRegistrar(
+  private val defaultEnabled: Boolean,
+) : ComponentRegistrar {
+
+  @Suppress("unused") // Used by service loader
+  constructor() : this(
+    defaultEnabled = true,
+  )
+
+  override fun registerProjectComponents(
+    project: MockProject,
+    configuration: CompilerConfiguration
+  ) {
+    val enabled = configuration.get(ExposeCommandLineProcessor.ARG_ENABLED, defaultEnabled)
+    if (enabled) {
+      IrGenerationExtension.registerExtension(project, ExposeIrGenerationExtension())
+    }
+  }
+}
+
+
